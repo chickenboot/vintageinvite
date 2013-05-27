7d3990d57586e43fa22fd00a50501c2fd31c4d2c
chickenboot <colin@chickenboot.com>
2013-05-27 18:46:17 +0100

    stage 5 - responsive custom top bar

--- content/assets/javascripts/app.js
+++ content/assets/javascripts/app.js
@@ -3,3 +3,9 @@
 $(document).ready(function () {
     $(document).foundationTopBar();
 });
+
+if (!Modernizr.svg) {
+  $('img[src*="svg"]').attr('src', function() {
+    return $(this).attr('src').replace('.svg', '.png');
+  });
+}
