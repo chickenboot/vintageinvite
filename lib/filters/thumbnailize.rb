dc310d72a7e54c1166272d8c92fc2b69e13f4877
chickenboot <colin@chickenboot.com>
2014-01-04 23:54:40 +0000

    add something to thumb the product pictures

---
+++ lib/filters/thumbnailize.rb
@@ -0,0 +1,17 @@
+class Thumbnailize < Nanoc::Filter
+  identifier :thumbnailize
+  type       :binary
+
+  def run(filename, params={})
+    dest_file = output_filename
+    dest_file += ".#{params[:extension]}" if params[:extension]
+    system(
+      'convert',
+      '-resize',
+      params[:width].to_s,
+      filename,
+      dest_file
+    )
+    File.rename(dest_file, output_filename) if params[:extension]
+  end
+end
