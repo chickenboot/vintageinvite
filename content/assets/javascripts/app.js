4ed1295537472f66044ac984bd0a8df86eef1b68
chickenboot <colin@chickenboot.com>
2014-02-17 06:21:59 +0000

    refine home and contact pages to new spec

--- content/assets/javascripts/app.js
+++ content/assets/javascripts/app.js
@@ -1,5 +1,13 @@
 <%= concat_files(@config[:javascripts][:compile]) %>
 
+$(document).foundation();
+
+$('[data-orbit]').on('orbit:after-slide-change', function(e, orbit_info) {
+  var activeSlide = $(e.currentTarget).find('li').eq(orbit_info.slide_number);
+  var currentImage = activeSlide.find('img');
+  $('#imageSource').html(currentImage[0].src);
+});
+
 $(document).ready(function () {
     $(document).foundationTopBar();
 });
