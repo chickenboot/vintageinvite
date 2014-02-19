c06d549d90b168be85013ac3510058398fbb2c6d
chickenboot <colin@chickenboot.com>
2014-02-19 21:17:14 +0000

    continued updates to match spec. bespoke page and some stuff on range

--- content/assets/javascripts/app.js
+++ content/assets/javascripts/app.js
@@ -1,7 +1,5 @@
 <%= concat_files(@config[:javascripts][:compile]) %>
 
-$(document).foundation();
-
 $('[data-orbit]').on('orbit:after-slide-change', function(e, orbit_info) {
   var activeSlide = $(e.currentTarget).find('li').eq(orbit_info.slide_number);
   var currentImage = activeSlide.find('img');
